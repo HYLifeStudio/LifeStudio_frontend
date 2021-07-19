cd18838455b4ba3b5fcff0dcaf41a932919a51f6
s2lululala <djm05190@gmail.com>
2021-07-20 00:46:49 +0900

    [feature] apply components to main.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route } from 'react-router-dom';
-import Header from "../component/header/header"
+import Header from "../component/header/header";
 import Footer from '../component/footer/footer';
 import MainNavbar from '../component/mainNavbar/mainNavbar';
 import SetLocation from '../component/setLocation/setLocation';
 import SearchStudio from '../component/searchStudio/searchStudio';
 import StudioType from '../component/studioType/studioType';
 import LocationMap from '../component/locationMap/locationMap';
+import AlertModal from "../component/alertmodal/alertModal";
+import { UserContext } from '../context/user';
 
 function Main(){
+    const {alertModal,setAlertModal} = useContext(UserContext);
+    function modalOpen(){
+        setAlertModal({onoff:true,msg:"hello"});
+    }
     return(
         <div>
             <Route component={Header}/>
@@ -17,7 +23,12 @@ function Main(){
             <Route component={SearchStudio}/>
             <Route component={StudioType}/>
             <Route component={LocationMap}/>
+            <div><button onClick={modalOpen}>모달</button></div>
             <Route component={Footer}/>
+            {
+                alertModal.onoff?
+                <AlertModal/>:<></>
+            }
         </div>
     )
 }
